0d15cf63fc372f21925d757057815f5df585c190
lukasimunovic <lukasimunovic2711@gmail.com>
2025-07-13 20:57:26 +0200

    JS Objekti...

---
+++ 8_Studenti/lsimunov/3_JS/V10_Funkcije/P1/script.js
@@ -0,0 +1,15 @@
+var words = ["quickest" , "brown" , "fox" , "jumps" , "over" , "lazy" , "dog"];
+
+// 1. Napišite JavaScript funkciju koja prihvaća zadani niz kao parametar i pronalazi najdužu riječ unutar niza.
+
+function findLongestWord(arr) {
+  var word = "";
+
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i].length > word.length) { 
+      word = arr[i];
+    }
+  }        
+  
+  return  word;
+}
